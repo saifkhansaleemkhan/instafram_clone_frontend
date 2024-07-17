05af8cea9e83498f19feda950d92966aaa91050f
saifkhansaleemkhan <saifk1177@gmail.com>
2024-07-18 01:37:43 +0530

    Add API for regisration

--- instander_ui/src/RegistrationPage/Register.jsx
+++ instander_ui/src/RegistrationPage/Register.jsx
@@ -1,28 +1,131 @@
-import React from 'react';
-import './Register.css'; // Import the SCSS file for styling
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Register.css'; // Import the CSS file for styling
 
 const Register = () => {
+  const [name, setName] = useState('');
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  const [errorMessage, setErrorMessage] = useState(''); // Add error message state
+  const [successMessage, setSuccessMessage] = useState(''); // Add success message state
+  const [countdown, setCountdown] = useState(5); // Add countdown state
+
+
+  useEffect(() => {
+    if (successMessage) {
+      const timer = setInterval(() => {
+        setCountdown((prevCountdown) => {
+          if (prevCountdown <= 1) {
+            clearInterval(timer);
+            navigate('/');
+            return 0;
+          }
+          return prevCountdown - 1;
+        });
+      }, 1000);
+
+      return () => clearInterval(timer);
+    }
+  }, [successMessage, navigate]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const userData = {
+      username,
+      email,
+      password
+    };
+
+    try {
+      const response = await fetch('https://instabookappserver.azurewebsites.net/api/user/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData),
+      });
+
+      const responseData = await response.json();
+
+      if (response.ok) {
+        // Redirect to the login page after successful registration
+        setSuccessMessage(responseData.message || 'Registration successful.');
+        setErrorMessage('');
+        // Redirect to the login page after 5 seconds
+        setTimeout(() => {
+          navigate('/');
+        }, 5000);
+      } else {
+        setErrorMessage(responseData.message || 'Registration failed. Please try again.');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   return (
     <div className="register-container">
       <div className="register-box">
         <h2>Registration for Instander</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form-group">
             <label htmlFor="name">Name</label>
-            <input type="text" id="name" name="name" required />
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
           </div>
           <div className="form-group">
             <label htmlFor="username">Username</label>
-            <input type="text" id="username" name="username" required />
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
           </div>
           <div className="form-group">
-            <label htmlFor="contact">Phone Number or Email</label>
-            <input type="text" id="contact" name="contact" required />
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" name="password" required />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
           </div>
+          {errorMessage && (
+            <div className="error-message" style={{ color: 'red' }}>
+              {errorMessage}
+            </div>
+          )}
+          {successMessage && (
+            <div className="success-message" style={{ color: 'green' }}>
+              {successMessage} Redirecting to login page in {countdown} seconds...
+            </div>
+          )}
+          <br/>
           <button type="submit">Register</button>
         </form>
         <p>
